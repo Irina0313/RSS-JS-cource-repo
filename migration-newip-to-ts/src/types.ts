baa3c35a26d6094865a46508736c7d51b53f81e9
Iryna Kanavalchuk <irkinbk@gmail.com>
2023-06-11 14:25:57 +0300

    feat: migrate sources.js to TS

--- migration-newip-to-ts/src/types.ts
+++ migration-newip-to-ts/src/types.ts
@@ -11,3 +11,13 @@ export interface NewsData {
     url: string;
     urlToImage: string;
 }
+
+export interface SourcesData {
+    category: string;
+    country: string;
+    description: string;
+    id: string;
+    language: string;
+    name: string;
+    url: string;
+}
