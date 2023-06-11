62361c5f501e086680cf21a1f294ef2be440a475
Iryna Kanavalchuk <irkinbk@gmail.com>
2023-06-11 14:04:52 +0300

    feat: migrate news.js to TS

--- migration-newip-to-ts/src/types.ts
+++ migration-newip-to-ts/src/types.ts
@@ -1,1 +1,13 @@
-export const firstName = 'nsme';
+export interface NewsData {
+    author: string;
+    content: string;
+    description: string;
+    publishedAt: string;
+    source: {
+        id: string;
+        name: string;
+    };
+    title: string;
+    url: string;
+    urlToImage: string;
+}
