a3822c1c0d6856f134f0c9083fc35c810d944401
Iryna Kanavalchuk <irkinbk@gmail.com>
2023-06-11 16:40:21 +0300

    feat: migrate appLoader.ts to TS

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '410f18727dc04e5eb572af77a70f1a4e', // получите свой ключ https://newsapi.org/
+            apiKey: '410f18727dc04e5eb572af77a70f1a4e' as string,
         });
     }
 }
