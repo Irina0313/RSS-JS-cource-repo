0db323e8e69beb1e82216539c87359a019e761e2
Iryna Kanavalchuk <irkinbk@gmail.com>
2023-06-12 14:20:33 +0300

    feat: add deploy script

--- migration-newip-to-ts/webpack.config.js
+++ migration-newip-to-ts/webpack.config.js
@@ -48,7 +48,7 @@ const baseConfig = {
     },
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, '../dist'),
+        path: path.resolve(__dirname, './dist'),
     },
     plugins: [
         new HtmlWebpackPlugin({
